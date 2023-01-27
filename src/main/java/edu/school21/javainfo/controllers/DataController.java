a67d725b54b0b08af071955c43c881283fe9f3a5
abceff <kunakov.rafael@gmail.com>
2023-01-27 16:40:32 +0300

    work in progress

--- src/main/java/edu/school21/javainfo/controllers/DataController.java
+++ src/main/java/edu/school21/javainfo/controllers/DataController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
+
 @Controller
 @RequestMapping("/data")
 public class DataController {
