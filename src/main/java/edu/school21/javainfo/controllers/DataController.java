15fb4c32ae23803fd75f087766ea6cef2d9c05cc
abceff <kunakov.rafael@gmail.com>
2023-02-04 20:27:25 +0300

    work in progress

--- src/main/java/edu/school21/javainfo/controllers/DataController.java
+++ src/main/java/edu/school21/javainfo/controllers/DataController.java
@@ -1,13 +1,11 @@
 package edu.school21.javainfo.controllers;
 
 import edu.school21.javainfo.dao.DAO;
-import edu.school21.javainfo.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.lang.reflect.Field;
+import edu.school21.javainfo.model.*;
 
 @Controller
 @RequestMapping("/data")
@@ -41,59 +39,68 @@ public class DataController {
     }
 
     @GetMapping("/{tablename}/insert")
-    public <T> String insert(@PathVariable("tablename") String tablename,
+    public String insert(@PathVariable("tablename") String tablename,
                              Model model) {
         model.addAttribute("object", dao.getObject(tablename));
         return "html/data/insert";
     }
 
-    @PostMapping("/checks/new")
-    public <T> String create(@ModelAttribute("object") Check check) {
+    @PostMapping("/checks/insert")
+    public String create(Check check) {
         dao.insert(check, "checks");
         return "redirect:/data/check/getall";
     }
-    @PostMapping("/friends/new")
-    public <T> String create(@ModelAttribute("object") Friend friend) {
+
+    @PostMapping("/friends/insert")
+    public String create(Friend friend) {
         dao.insert(friend, "friends");
         return "redirect:/data/friends/getall";
     }
-    @PostMapping("/p2p/new")
-    public <T> String create(@ModelAttribute("object") P2p p2p) {
+
+    @PostMapping("/p2p/insert")
+    public String create(P2p p2p) {
         dao.insert(p2p, "p2p");
         return "redirect:/data/p2p/getall";
     }
-    @PostMapping("/peers/new")
-    public <T> String create(@ModelAttribute("object") Peer peer) {
+
+    @PostMapping("/peers/insert")
+    public String create(Peer peer) {
         dao.insert(peer, "peers");
         return "redirect:/data/peers/getall";
     }
-    @PostMapping("/recommendations/new")
-    public <T> String create(@ModelAttribute("object") Recommendation recommendation) {
+
+    @PostMapping("/recommendations/insert")
+    public String create(Recommendation recommendation) {
         dao.insert(recommendation, "recommendations");
         return "redirect:/data/recommendations/getall";
     }
-    @PostMapping("/tasks/new")
-    public <T> String create(@ModelAttribute("object") Task task) {
+
+    @PostMapping("/tasks/insert")
+    public String create(Task task) {
         dao.insert(task, "tasks");
         return "redirect:/data/tasks/getall";
     }
-    @PostMapping("/timetracking/new")
-    public <T> String create(@ModelAttribute("object") Timetracking timetracking) {
+
+    @PostMapping("/timetracking/insert")
+    public String create(Timetracking timetracking) {
         dao.insert(timetracking, "timetracking");
         return "redirect:/data/timetracking/getall";
     }
-    @PostMapping("/transferredpoints/new")
-    public <T> String create(@ModelAttribute("object") Transferredpoint transferredpoint) {
+
+    @PostMapping("/transferredpoints/insert")
+    public String create(Transferredpoint transferredpoint) {
         dao.insert(transferredpoint, "transferredpoints");
         return "redirect:/data/transferredpoints/getall";
     }
-    @PostMapping("/verter/new")
-    public <T> String create(@ModelAttribute("object") Verter verter) {
+
+    @PostMapping("/verter/insert")
+    public String create(Verter verter) {
         dao.insert(verter, "verter");
         return "redirect:/data/verter/getall";
     }
-    @PostMapping("/xp/new")
-    public <T> String create(@ModelAttribute("object") Xp xp) {
+
+    @PostMapping("/xp/insert")
+    public String create(Xp xp) {
         dao.insert(xp, "xp");
         return "redirect:/data/xp/getall";
     }
